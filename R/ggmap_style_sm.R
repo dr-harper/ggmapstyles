16ac0642d1fe285d780491d6af0fd2b617f28c6f
Michael Harper <mikey-harper@hotmail.com>
2018-06-21 20:47:31 +0100

    Update to package

--- R/ggmap_style_sm.R
+++ R/ggmap_style_sm.R
@@ -1,7 +1,10 @@
-#' Create a style string for
+#' Create a google maps style string
+#' 
+#' Provided with a SnazzyMap ID, this function will return a formatted stylestring
+#'  that can be used within the \code{get_googlemap} function.
 #' 
 #' @param mapRef the full Snazzymap page URL, or the ID of the map layer
-#' 
+#' @seealso \code{\link[ggmap]{get_googlemap}}
 #' @export
 #' 
 ggmap_style_sm <- function(mapRef){
